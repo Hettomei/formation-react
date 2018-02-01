1b75998729d3524a3527e743a1fe93436ed3335b
Timothee GAUTHIER <timothee.gauthier@consertotech.pro>
2018-02-01 16:18:16 +0100

    Add link to official docs

--- src/Header.js
+++ src/Header.js
@@ -2,28 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import {
-  Navbar,
   Nav,
-  NavItem,
   NavDropdown,
+  NavItem,
+  Navbar,
 } from 'react-bootstrap';
 
 function Header(props) {
   return <Navbar inverse fluid>
+
     <Navbar.Header>
       <Navbar.Brand>
         <Link to='/'>Formation React</Link>
       </Navbar.Brand>
     </Navbar.Header>
+
     <Nav>
       <LinkContainer to="/readme">
         <NavItem>A-propos</NavItem>
       </LinkContainer>
+
       <NavItem href='https://github.com/Hettomei/react-budget'>Sources</NavItem>
-      <NavDropdown title="Documentation React" id="basic-nav-dropdown">
+
+      <NavDropdown title="React" id="basic-nav-dropdown">
         <NavItem href='https://reactjs.org/'>reactjs.org</NavItem>
         <NavItem href='https://github.com/facebook/create-react-app'>Create React apps</NavItem>
+        <NavItem href='https://github.com/facebook/flux'>flux implementation</NavItem>
       </NavDropdown>
+
+      <NavDropdown title="Redux" id="basic-nav-dropdown">
+        <NavItem href='https://redux.js.org/'>redux.js.org</NavItem>
+      </NavDropdown>
+
+      <NavDropdown title="React-router" id="basic-nav-dropdown">
+        <NavItem href='https://reacttraining.com/react-router/'>react-router</NavItem>
+      </NavDropdown>
+
     </Nav>
   </Navbar>
 }
