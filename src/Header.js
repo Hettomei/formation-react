df5eb12cb31d6a0972217f93e933cd12ceca0343
Timothee GAUTHIER <timothee.gauthier@consertotech.pro>
2018-02-01 10:17:08 +0100

    Add official documentation

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+} from 'react-bootstrap';
 import linksRoutesComponents from './linksRoutesComponents';
 
 function Header(props) {
@@ -11,9 +16,11 @@ function Header(props) {
       </Navbar.Brand>
     </Navbar.Header>
     <Nav>
-      <NavItem href='https://github.com/Hettomei/react-budget'>
-        sources
-      </NavItem>
+      <NavItem href='https://github.com/Hettomei/react-budget'>sources</NavItem>
+      <NavDropdown title="Documentation React" id="basic-nav-dropdown">
+        <NavItem href='https://reactjs.org/'>Home</NavItem>
+        <NavItem href='https://github.com/facebook/create-react-app'>Create React apps</NavItem>
+      </NavDropdown>
     </Nav>
   </Navbar>
 }
