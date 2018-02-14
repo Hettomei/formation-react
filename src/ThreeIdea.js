fee8fdc1cdf926bdbe0780e79e45b16a0429f08e
Timothee GAUTHIER <timothee.gauthier@consertotech.pro>
2018-02-14 21:31:19 +0100

    Better 3 idea page

--- src/ThreeIdea.js
+++ src/ThreeIdea.js
@@ -3,19 +3,26 @@ import {
   Image,
 } from 'react-bootstrap';
 
-import flux from './img/flux-diagram-white-background.png';
 import jsx from './img/jsx.png';
+import flux from './img/flux-diagram-white-background.png';
+import reconciliation from './img/reconciliation.gif';
 
 const ThreeIdea = () => (
   <div>
     <h2>3 idées</h2>
-    <ul>
-      <li><a href='https://reactjs.org/docs/jsx-in-depth.html'>JSX</a> 'it is just syntactic sugar' : on peut s'en passer.</li>
-      <li>One-way data flow (<a href='https://github.com/facebook/flux'>flux implementation</a>, then <a href='https://redux.js.org/'>redux</a>)</li>
-      <li>Virtual DOM / <a href='https://reactjs.org/docs/reconciliation.html'>reconcilliation</a></li>
-    </ul>
-    <p><Image src={jsx} responsive /><a href='https://reactjs.org/docs/jsx-in-depth.html'>Jsx in depth</a></p>
+
+    <h3>1) <a href='https://reactjs.org/docs/jsx-in-depth.html'>JSX</a></h3>
+    <p>'It is just syntactic sugar' : on peut s'en passer.</p>
+    <p><Image src={jsx} responsive /></p>
+
+    <h3>2) One-way data flow</h3>
+    <p>(<a href='https://github.com/facebook/flux'>flux implementation</a>, puis <a href='https://redux.js.org/'>redux</a>)</p>
     <p><Image src={flux} responsive /><a href='https://github.com/facebook/flux'>Credit: https://github.com/facebook/flux</a></p>
+
+    <h3>3) Virtual DOM</h3>
+    <p>où '<a href='https://reactjs.org/docs/reconciliation.html'>réconciliation</a>'</p>
+    <p><Image src={reconciliation} responsive /><a href='https://reactjs.org/docs/rendering-elements.html'>Credit: https://reactjs.org/docs/rendering-elements.html</a></p>
+
   </div>
 )
 
