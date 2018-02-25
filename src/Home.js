3c0ab9d174a4a5518424363ffc1f6ed3bc8df405
Timothee GAUTHIER <timothee.gauthier@consertotech.pro>
2018-02-25 21:55:15 +0100

    Add react logo

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './shared/consertoLogo.svg'; // Tell Webpack this JS file uses this image
+import reactIcon from './shared/React-icon.svg'; // Tell Webpack this JS file uses this image
 import email from './constant/email';
 
 const Home = () => (
@@ -7,7 +8,7 @@ const Home = () => (
     <div className="text-center">
       <h2><img className="home-logo" src={logo} alt="Logo" /></h2>
       <h1>-</h1>
-      <h1>Formation React</h1>
+      <h1>Formation React<img style={ { width: '140px' } } className="react-logo" src={reactIcon} alt="react Logo" /></h1>
     </div>
     <p>{email}</p>
   </div>
